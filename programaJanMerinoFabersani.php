bd24ef55675aa048718bb1de8cdb09b0ff448cbc
JoseJan21 <josejan303@gmail.com>
2023-11-15 15:33:44 -0300

    Agregando funciones 8 y 10

--- programaJanMerinoFabersani.php
+++ programaJanMerinoFabersani.php
@@ -43,7 +43,7 @@ function cargarColeccionPalabras()
 //FUNCION 3
 /**
  * Muestra las opciones del menú en pantalla y le solicita al usuario una opcion
- * @return Integer
+ * @return int
  */
 function seleccionarOpcion(){
     
@@ -67,9 +67,6 @@ function seleccionarOpcion(){
 //FUNCION 4
 
 
-//FUNCION 5
-
-
 //FUNCION 6
 
 
@@ -77,13 +74,61 @@ function seleccionarOpcion(){
 
 
 //FUNCION 8
+/**
+ * Recibe por parametro una colección de partidas y el nombre de un jugador,
+ * retorna el índice de la primer partida ganada por dicho jugador.
+ * Si el jugador no ganó ninguna partida, la función retorna -1.
+ * @param array $coleccionPartidas
+ * @param string $jugador
+ * @return int
+ */
+function primerPartidaGanada($coleccionPartidas, $jugador){
 
+    // int $indice
+
+    $indice = -1;
+
+    // Iteramos sobre cada partida en la colección.
+    foreach ($coleccionPartidas as $clave => $partida){
+        /* Asegura si el jugador de la partida es el jugador que estamos buscando
+        y si el puntaje de la partida es mayor que cero (lo que indica que la partida fue ganada)*/
+        if ($partida['jugador'] == $jugador && $partida['puntaje'] > 0){
+            $indice = $clave; // Actualizamos el índice con el valor actual.
+            break; // Terminamos el bucle cuando encontramos la primera partida ganada.
+        }
+    }
+
+    return $indice;
+}
 
 //FUNCION 9
 
 
 //FUNCION 10
+/**
+ * Solicita al usuario el nombre de un jugador y retorna el nombre en minúsculas.
+ * Y asegura que el nombre del jugador comience con una letra.
+ * @return str
+ */
+function solicitarJugador(){
+    
+    //int $nombreJugador
 
+    echo "Ingrese el nombre del jugador: \n";
+    $nombreJugador = trim(fgets(STDIN));
+
+    /* Asegura que el nombre ingresado comience con una letra,
+    el bucle seguirá pidiendo al usuario que ingrese el nombre hasta que cumpla con esta condición.*/
+    while (!ctype_alpha($nombreJugador[0])) {
+        echo "El nombre del jugador NO comienza con una letra. Ingrese de nuevo: \n";
+        $nombreJugador = trim(fgets(STDIN));
+    }
+
+    // Funcion strtolower: Convierte el nombre a minúsculas
+    $nombreJugador = strtolower($nombreJugador);
+
+    return $nombreJugador;
+}
 
 //FUNCION 11
 
